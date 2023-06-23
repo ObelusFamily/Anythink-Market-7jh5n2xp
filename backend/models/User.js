ce793cac4a137b606fcc94ec88a552099caaf145
Akuya Ekorot <akuyaekorot@gmail.com>
2023-06-23 07:32:48 +0000

    Revert "add isVerified field to user model and /items api endpoint'"
    This reverts commit 44c6078f8f99134547fdb19609bbbb393b77c9bd.

--- backend/models/User.js
+++ backend/models/User.js
@@ -6,7 +6,6 @@ var secret = require("../config").secret;
 
 var UserSchema = new mongoose.Schema(
   {
-    isVerified: Boolean,
     username: {
       type: String,
       lowercase: true,
@@ -76,8 +75,7 @@ UserSchema.methods.toAuthJSON = function() {
     token: this.generateJWT(),
     bio: this.bio,
     image: this.image,
-    role: this.role,
-    isVerified: this.isVerified
+    role: this.role
   };
 };
 
@@ -87,8 +85,7 @@ UserSchema.methods.toProfileJSONFor = function(user) {
     bio: this.bio,
     image:
       this.image || "https://static.productionready.io/images/smiley-cyrus.jpg",
-    following: user ? user.isFollowing(this._id) : false,
-    isVerified: this.isVerified
+    following: user ? user.isFollowing(this._id) : false
   };
 };
 
